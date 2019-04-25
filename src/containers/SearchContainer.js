61d868c8ff4e0cc27980942bdebabcac061e5b73
kszeto1 <kenny.szeto1@gmail.com>
2019-04-25 12:34:47 -0700

    Pass specs through search container

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,53 @@ import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleSearchChange from '../actions/search.js';
 
-var SearchContainer = () => {};
+var SearchContainer = () => {
 
-//TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
-//HINT: use react-redux 'connect' method to generate a container component from
-//state and dispatch mappings.
+  //TODO: define a SearchContainer component which will hook up your action
+  // dispatchers with your search component props.
+  //HINT: use react-redux 'connect' method to generate a container component from
+  //state and dispatch mappings.
 
-export default SearchContainer;
+  /*
+  define a searchContainer component
+    that uses Search component
+    our action dispatcher is handleSearchChange
+      somehow using connect
+
+      dispatch(handleSearchChange)
+      want export connect()(Search)
+
+      need to use mapStateToProps
+        const mapStateToProps = (state) => {
+      return {
+          items: state.items,
+          hasErrored: state.itemsHasErrored,
+          isLoading: state.itemsIsLoading
+        };
+      };
+
+      need to use mapDispatchToProps
+        const mapDispatchToProps = (dispatch) => {
+      return {
+          fetchData: (url) => dispatch(itemsFetchData(url))
+          };
+      };
+
+      return connect(mapstateprops, mapdispatchprops)(search)
+  */
+  const mapStateToProps = (state) => {
+    return {
+      value: state.handleSearchChange
+    };
+  };
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      handleSearchInputChange: (q) => dispatch(handleSearchChange(q))
+    };
+  };
+
+  return connect(mapStateToProps, mapDispatchToProps)(Search)
+};
+
+export default SearchContainer();
--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
-import configureStore from './store/configureStore.js';
+// import configureStore from './store/configureStore.js';
 
 //TODO: Import the Provider component from 'react-redux' here!
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
 
-const store = configureStore();
+// const store = configureStore();
 
 ReactDOM.render(
   <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
   document.getElementById('app')
 );
+
+/*
+const store = configureStore(); // You can also pass in an initialState here
+
+render(
+    <Provider store={store}>
+        <ItemList />
+    </Provider>,
+    document.getElementById('app')
+);
+*/
