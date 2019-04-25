9d621b623d232ba4e2c92a0543a59d7e40d376b8
kszeto1 <kenny.szeto1@gmail.com>
2019-04-24 20:35:08 -0700

    Added code up to store. Passes mocha tests.

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,24 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  var options = {
+    key: YOUTUBE_API_KEY,
+    query: q,
+    max: 5
+  };
+  return (dispatch) => {
+    searchYouTube(options, (d) => {
+      dispatch(changeVideoList(d))
+      dispatch(changeVideo(d[0]))
+      // return {
+      //   type: 'SEARCH EVENT',
+
+      // }
+    })
+  }
+
 };
 
 export default handleVideoSearch;
